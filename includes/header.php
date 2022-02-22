214f49032f155c67f280b0067c1962da6473d811
joseph-benoy <josephbenoy03@gmail.com>
2022-02-22 18:57:15 +0530

    home paeg created

---
+++ includes/header.php
@@ -0,0 +1,8 @@
+<header class="topnav">
+    <a href="#news" style='font-weight:bold'>JOBS ONLINE</a>
+    <span>
+        <a class="active" href="#home">Home</a>
+        <a href="#news">Jobs</a>
+        <a href="#contact">Login</a>
+    </span>
+</header>
---
+++ index.php
@@ -0,0 +1,18 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Jobs Online</title>
+    <link rel="stylesheet" href="style.css"/>
+</head>
+<body>
+    <?php include("includes/header.php");?>
+    <img src="images/home1.png" id="hero">
+    <div class="home-hero">
+        <h1>Find the job that fits your life</h1>
+        <a href="#">Find Jobs</a>
+    </div>
+</body>
+</html>
